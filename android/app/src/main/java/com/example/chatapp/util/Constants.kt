29c6e86f4561e39629fea095157dad1bee8c649a
Hissah-saleh <Hessah.s.al@gmail.com>
2025-06-29 17:21:25 +0300

    - remove runtime error

--- android/app/src/main/java/com/example/chatapp/util/Constants.kt
+++ android/app/src/main/java/com/example/chatapp/util/Constants.kt
@@ -1,7 +1,6 @@
 package com.example.chatapp.util
 
 object Constants {
-    val CHAT_SERVER_URL= getYourOwnDeviceIpAddress()
     const val TYPING= "typing"
     const val STOP_TYPING= "stop typing"
     const val LOGIN= "login"
